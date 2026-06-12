cf42ce68d701b4678c17c102ef232ec8c70c97a5
pozercpp <n.a.chigrin@yandex.ru>
2026-06-13 01:57:06 +0300

    Change getAllFiles parameter to string_view
    Updated getAllFiles to use string_view for improved performance.

--- src/utils/FileSystem.cpp
+++ src/utils/FileSystem.cpp
@@ -1,16 +1,17 @@
 #include<vector>
 #include<string>
+#include <string_view>
 #include<filesystem>
 
 #include<FileSystem.hpp>
 
-void FileSystem::getAllFiles(const std::string& dir, std::vector<std::string>& files) {
+void FileSystem::getAllFiles(std::string_view dir, std::vector<std::string>& files) {
     for (const auto& file : std::filesystem::directory_iterator(dir)) {
         if (file.is_directory()) {
             getAllFiles(file.path().string(), files);
         } else if (file.is_regular_file()) {
             std::string ext = file.path().extension().string();
-            auto Checktxt = [&](const std::string& file) -> bool {
+            auto Checktxt = [](const std::string_view ext) -> bool {
                 return (ext == ".txt" || ext == ".md" || ext == ".html" || ext == ".log" || ext == ".doc");
             };
             if (Checktxt(ext)) {
